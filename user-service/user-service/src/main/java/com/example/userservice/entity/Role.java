0ab51ebaca11b9e18395d514440b610f6db52687
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-04-26 00:30:30 +0530

    Commit

---
+++ user-service/user-service/src/main/java/com/example/userservice/entity/Role.java
@@ -0,0 +1,8 @@
+package com.example.userservice.entity;
+
+public enum Role {
+    ADMIN,
+    INSTRUCTOR,
+    LEARNER
+}
+
---
+++ user-service/user-service/src/main/java/com/example/userservice/service/Impl/UserAuthenticationService.java
@@ -0,0 +1,36 @@
+package com.example.userservice.service.Impl;
+
+
+import com.example.userservice.entity.User;
+import com.example.userservice.entity.UserAuthenticationDetails;
+import com.example.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserAuthenticationService implements UserDetailsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserAuthenticationService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        if(user == null){
+            logger.error("User Not Found");
+            throw new UsernameNotFoundException("User Not Found");
+
+        }
+        return new UserAuthenticationDetails(user);
+    }
+}
