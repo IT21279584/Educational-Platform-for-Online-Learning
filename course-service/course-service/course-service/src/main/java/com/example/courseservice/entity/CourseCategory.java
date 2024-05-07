a7d43d3f939263fecbee0ea5c2b118b79cc00973
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-05-07 21:31:51 +0530

    Update Course Model and controller

---
+++ course-service/course-service/course-service/src/main/java/com/example/courseservice/entity/CourseCategory.java
@@ -0,0 +1,2 @@
+package com.example.courseservice.entity;public record CourseCategory() {
+}
---
+++ course-service/course-service/course-service/src/main/java/com/example/courseservice/entity/CourseType.java
@@ -0,0 +1,8 @@
+package com.example.courseservice.entity;
+
+public enum Category {
+
+    BEGINNER_LEVEL,
+    INTERMIDIATE_LEVEL,
+    ADVANCED_LEVEL
+}
