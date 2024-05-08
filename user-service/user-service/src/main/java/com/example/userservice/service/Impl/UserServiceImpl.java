6271220b8e2a96f308cd021cf29dd93f7d0c99c9
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-05-08 19:36:17 +0530

    update user service

--- user-service/user-service/src/main/java/com/example/userservice/service/Impl/UserServiceImpl.java
+++ user-service/user-service/src/main/java/com/example/userservice/service/Impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
