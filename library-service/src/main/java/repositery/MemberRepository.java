da03f7d9ab0eb77a7d7f6fd7fac43cbd473d3f01
Tharanee De Silva <it21219702@my.sliit.lk>
2024-05-10 15:08:40 +0530

    update1

--- library-service/src/main/java/repositery/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package repositery;
-
-import entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Member,Integer> {
-}
